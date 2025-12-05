4424d2b2993a63ea6aa1aabe4c7631dd782ae6d8
Thanooss <115245988+Thanooss@users.noreply.github.com>
2025-12-05 19:30:15 +0530

    Refactor MySQL connection to use environment variables
    Updated MySQL connection configuration to use environment variables.

--- server/server.js
+++ server/server.js
@@ -16,10 +16,11 @@ app.use(session({
 
 // Configure MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Stoner@69',
-  database: 'world'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 // Connect to MySQL
